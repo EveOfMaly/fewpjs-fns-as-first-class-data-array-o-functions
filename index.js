f5460ab8ef3206a74df158a22f632e6b22665b61
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-22 18:18:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
  //functions related to dog care
-  function wakeDog (dogName, dogBreed) {
+  function wakeDog (dogName = "Xerox", dogBreed = "Golden Retriever") {
     console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`;
   }
   
-  function LeashDog (dogName, dogBreed) {
-    console.log(`Lease ${dogName} the ${dogBreed}`);
+  function leashDog (dogName = "Boo Radley", dogBreed = "Pibble") {
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`;
   }
   
-  function walkToPark (dogName, dogBreed) {
-    console.log(`Walk to the part with ${dogName} the ${dogBreed}`);
+  function walkToPark (dogName = "Xerox", dogBreed = "Golden Retriever") {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`;
   }
   
-  function throwFrisbee (dogName, dogBreed) {
+  function throwFrisbee (dogName = "Xerox", dogBreed = "Golden Retriever") {
     console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`;
   }
   
-  function walkHome (dogName, dogBreed) {
-    console.log(`Wake home with ${dogName} the ${dogBreed}`);
+  function walkHome (dogName = "Xerox", dogBreed = "Golden Retriever") {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`;
   }
   
-  function unleashDog (dogName, dogBreed) {
+  function unleashDog (dogName = "Xerox", dogBreed = "Golden Retriever") {
     console.log(`Unleash ${dogName} the ${dogBreed}`);
+    return `Unleash ${dogName} the ${dogBreed}`;
   }
 
   //arrray of functions
-  const routine = [wakeDog, LeashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+  const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
 
   //function to process the array of functions
 
@@ -33,4 +39,4 @@
      return responses;
   }
 
-  exerciseDog ('Xerox', "Border Collie");
+wakeDog('Xerox', "Border Collie");
